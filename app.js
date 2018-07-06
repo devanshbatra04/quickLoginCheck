16de811f995ac8e62068c79526bf3d4dab3d0278
Devansh Batra <devanshbatra04@gmail.com>
2018-07-06 20:39:23 +0530

    json parsed

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app               = express();
 
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");

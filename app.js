28d90601ad0b68cfd128fa0947453e50a855151a
Devansh Batra <devanshbatra04@gmail.com>
2018-07-06 19:58:22 +0530

    cors fix

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.post('/login', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
     if (
         req.body.email === 'chuck@example.com' && req.body.password === 'Norris@1234'
     ) res.sendStatus(200);

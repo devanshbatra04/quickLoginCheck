69c3f21fcf59d526a55e5a79f9c388bf119d6c39
Devansh Batra <devanshbatra04@gmail.com>
2018-07-06 14:14:50 +0530

    login post route" ;

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const app               = express();
 
 app.set('view engine', 'ejs');
 
+app.post('/login', (req, res) => {
+    if (
+        req.body.email === 'chuck@example.com' && req.body.password === 'Norris@1234'
+    ) res.sendStatus(200);
+    else {
+        res.sendStatus(406);
+    }
+
+})
+
 app.get('/', (req,res) => res.render('index'));
 
 let port = process.env.PORT || 4000,

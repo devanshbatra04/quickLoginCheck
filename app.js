9f3821541068f0ac89d00e94d94760060fb74cdd
Devansh Batra <devanshbatra04@gmail.com>
2018-07-06 14:05:59 +0530

    EJS and root route

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express           = require('express');
 const app               = express();
 
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req,res) => res.render('index'));
+
 let port = process.env.PORT || 4000,
     ip   = '0.0.0.0';
 
